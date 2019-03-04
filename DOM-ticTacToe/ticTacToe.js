7b76e893c125a06f42d85a86c959fd76d7ebcb81
daniela-idara <purpleelephant1987@gmail.com>
2019-03-03 18:52:34 -0600

    update X and O buttons

--- DOM-ticTacToe/ticTacToe.js
+++ DOM-ticTacToe/ticTacToe.js
@@ -1,21 +1,22 @@
-var gameMarker = "X"
+alert("Player 1: starts with X.\nPlayer 2: Select 0 to change marker.\nClick Button to change marker until the end!")
+
 var gameMarker = "O"
+var gameMarker = "X"
+
+function changeMarkerToX() {
+  gameMarker = "X";
+  console.log("The X button was clicked!");
+}
+
+function changeMarkerToO() {
+  gameMarker = "O";
+  console.log("The O button was clicked!");
+}
 
 
-function changeMarkerToX(){ 
-     gameMarker = "X";
-    console.log("The X button was clicked!");
-} 
-
-function changeMarkerToO(){
-     gameMarker = "O";
-    console.log("The O button was clicked!");
- }
-
-
- var buttons = document.querySelectorAll('.square');
- for (var button of buttons) {
-  button.addEventListener('click', function() {
+var buttons = document.querySelectorAll('.square');
+for (var button of buttons) {
+  button.addEventListener('click', function () {
     console.log("click");
     this.innerHTML = gameMarker;
   });
